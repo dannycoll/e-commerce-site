afe4b6677509e315c72ef2996be27f7353caac69
Daniel Collins <dptc98@gmail.com>
2020-12-22 11:42:20 +0000

    Registration placeholder & routes implemented

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import './default.scss';
+import { Switch, Route } from 'react-router-dom';
 
 import Header from './components/Header';
 import Homepage from './pages/Homepage';
+import Registration from './pages/Registration';
+
 function App() {
   return (
     <div className="App">
       <Header />
       <div className="main">
-        <Homepage />
+        <Switch>
+          <Route exact path={'/'} component={Homepage}/>
+          <Route path={'/registration'} component={Registration}/>
+        </Switch>
       </div>
     </div>
   );
