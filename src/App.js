845410be7946c2f6ad717c5d2229025e2cd774b5
Daniel Collins <dptc98@gmail.com>
2020-12-23 16:55:07 +0000

    Routing & Custom Layouts Added

--- src/App.js
+++ src/App.js
@@ -2,20 +2,24 @@ import React from 'react';
 import './default.scss';
 import { Switch, Route } from 'react-router-dom';
 
-import Header from './components/Header';
+import MainLayout from './layouts/MainLayout';
+import HomepageLayout from "./layouts/HomepageLayout";
 import Homepage from './pages/Homepage';
 import Registration from './pages/Registration';
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <div className="main">
-        <Switch>
-          <Route exact path={'/'} component={Homepage}/>
-          <Route path={'/registration'} component={Registration}/>
-        </Switch>
-      </div>
+      <Switch>
+        <Route exact path={'/'} render={() => (
+          <HomepageLayout>
+            <Homepage />
+          </HomepageLayout>)}/>
+        <Route path={'/registration'} render={() => (
+          <MainLayout>
+            <Registration />
+          </MainLayout>)}/>
+      </Switch>
     </div>
   );
 }
