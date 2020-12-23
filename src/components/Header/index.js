e084b9554f819753b4bb036df83863f985f8707a
Daniel Collins <dptc98@gmail.com>
2020-12-23 17:05:47 +0000

    Link to registration page added

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import './styles.scss';
 import Logo from '../../assets/LogoTemp.png';
+import { Link } from "react-router-dom";
 const Header = props => {
     return (
         <header className="header">
             <div className="wrap">
                 <div className="logo">
-                    <img src={Logo} style={{height: "50px" }} alt="Logo here"/>
+                    <Link to="/">
+                        <img src={Logo} style={{height: "50px" }} alt="Logo here"/>
+                    </Link>
+                </div>
+
+                <div className="callToActions">
+                    <ul>
+                        <li>
+                            <Link to="/registration">
+                                Register
+                            </Link>
+                        </li>
+                    </ul>
                 </div>
             </div>
         </header>
